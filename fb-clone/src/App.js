b1f797688201f76e744be6cc97a6f58ce636ce25
Noor <noormir1295@gmail.com>
2020-09-19 19:20:19 -0400

    adding neccesary files

---
+++ fb-clone/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './App.css';
+
+function app() {
+  return (
+    <div className="App">
+    <h1>Lets build a clone</h1>
+    </div>
+  );
+}
+
+export default app;
