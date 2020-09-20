2790b86d0782c79d640463d5ab5050a9d23472c4
Noor <noormir1295@gmail.com>
2020-09-19 23:37:20 -0400

    updating sidebar js

--- fb-clone/src/App.js
+++ fb-clone/src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import Header from "./header";
+import Header from "./Header";
 import "./App.css";
 import Sidebar from "./Sidebar";
 
--- fb-clone/src/Sidebar.js
+++ fb-clone/src/Sidebar.js
@@ -1,12 +1,28 @@
 import React from "react";
 import "./Sidebar.css";
 import SidebarRow from "./SidebarRow";
+import EmojiFlagsIcon from "@material-ui/icons/EmojiFlags";
+import PeopleIcon from "@material-ui/icons/People";
+import ChatIcon from "@material-ui/icons/Chat";
+import StorefrontIcon from "@material-ui/icons/Storefront";
+import VideoLibraryIcon from "@material-ui/icons/VideoLibrary";
+import LocalHospitalIcon from "@material-ui/icons/LocalHospital";
+import { ExpandMoreOutlined } from "@material-ui/icons";
 
 function Sidebar() {
   return (
     <div className="sidebar">
-      <SidebarRow />
-      <SidebarRow />
+      <SidebarRow src="" title="Noor Mir" />
+      <SidebarRow
+        Icon={LocalHospitalIcon}
+        title="COVID_19 Information Center"
+      />
+      <SidebarRow Icon={EmojiFlagsIcon} title="Pages" />
+      <SidebarRow Icon={PeopleIcon} title="Friends" />
+      <SidebarRow Icon={ChatIcon} title="Messenger" />
+      <SidebarRow Icon={StorefrontIcon} title="Marketplace" />
+      <SidebarRow Icon={VideoLibraryIcon} title="Videos" />
+      <SidebarRow Icon={ExpandMoreOutlined} title="Pages" />
     </div>
   );
 }
