79cf7726a614ec17a8ecc5ed70ba5c286dcc5481
Noor <noormir1295@gmail.com>
2020-09-20 19:54:01 -0400

    adding storyreel in feed section

--- fb-clone/src/App.js
+++ fb-clone/src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import Header from "./Header";
 import "./App.css";
 import Sidebar from "./Sidebar";
+import Feed from "./Feed.js"
 
 function App() {
   return (
---
+++ fb-clone/src/StoryReel.js
@@ -0,0 +1,38 @@
+import React from 'react'
+import "./StoryReel.css"
+import Story from "./Story";
+
+function StoryReel() {
+    return (
+      <div className="storyReel">
+        <Story
+          image="https://scontent.fmia1-2.fna.fbcdn.net/v/t31.0-8/15384522_10211261259593171_8435885993263401506_o.jpg?_nc_cat=105&_nc_sid=a9b1d2&_nc_ohc=LvtXB1HQv9IAX97X7aB&_nc_ht=scontent.fmia1-2.fna&oh=8ca957375e756020ebe74faa00698a78&oe=5F8CF669"
+          profileSrc="https://avatars2.githubusercontent.com/u/66847899?s=460
+        &u=d480012dd0ea3b716a77269ea7db462baf291261&v=4"
+          title="Noor Mir"
+        />
+        <Story
+          image="https://scontent.fmia1-1.fna.fbcdn.net/v/t1.0-9/11392975_10155642076705223_3576715787895802679_n.jpg?_nc_cat=101&_nc_sid=a9b1d2&_nc_ohc=_f1syXJRXAUAX8eKErv&_nc_ht=scontent.fmia1-1.fna&oh=3c71b2c7d9b9d0dff651cd8e9f659626&oe=5F8D208D"
+          profileSrc="https://scontent.fmia1-1.fna.fbcdn.net/v/t31.0-8/1658195_10153836833980223_2012324332_o.jpg?_nc_cat=106&_nc_sid=174925&_nc_ohc=VeZXJ7btm90AX8D-Bfk&_nc_ht=scontent.fmia1-1.fna&oh=ae0b44f242d39d2acd4d66c91ce16544&oe=5F8C974A"
+          title="Zain Mir"
+        />
+        <Story
+          image="https://scontent.fmia1-1.fna.fbcdn.net/v/t31.0-8/18402194_10209301796317543_9105831440003409711_o.jpg?_nc_cat=102&_nc_sid=a9b1d2&_nc_ohc=lsxnt39PNFUAX-AJ4ve&_nc_ht=scontent.fmia1-1.fna&oh=6919fa87a531bb67df88e580ec53527c&oe=5F8D426B"
+          profileSrc="https://scontent.fmia1-2.fna.fbcdn.net/v/t1.0-9/16730667_10208622764982184_552602404117941872_n.jpg?_nc_cat=104&_nc_sid=174925&_nc_ohc=O8deC8JWunsAX_UZaR5&_nc_ht=scontent.fmia1-2.fna&oh=2b084a893d5d29e402b2eb1cac2155d9&oe=5F8B9C37"
+          title="Omar Mirza"
+        />
+        <Story
+          image="https://scontent.fmia1-2.fna.fbcdn.net/v/t1.0-9/26166480_10215472469228685_1404890729860351777_n.jpg?_nc_cat=109&_nc_sid=8bfeb9&_nc_ohc=TFOnuS6GmzsAX-kZ98E&_nc_ht=scontent.fmia1-2.fna&oh=91d68188fafb5ac0ff77217607e4c1a3&oe=5F8E7C68"
+          profileSrc="https://scontent.fmia1-2.fna.fbcdn.net/v/t1.0-9/18011127_10213065867625149_8773596060351402133_n.jpg?_nc_cat=104&_nc_sid=174925&_nc_ohc=5f9KcY-emEMAX9LdwOQ&_nc_ht=scontent.fmia1-2.fna&oh=b26498f4af71fcdad507b323c6fc4282&oe=5F8C8005"
+          title="Asham Amir"
+        />
+        <Story
+          image="https://scontent.fmia1-2.fna.fbcdn.net/v/t1.0-9/12391778_10205775962335079_3767409845134795048_n.jpg?_nc_cat=111&_nc_sid=174925&_nc_ohc=x_73WQ6z040AX908MWo&_nc_ht=scontent.fmia1-2.fna&oh=546a1cb9662907f6ab6d172a945b308e&oe=5F8B8206"
+          profileSrc="https://scontent.fmia1-1.fna.fbcdn.net/v/t1.0-9/23472123_10210933305785442_3134189413193463440_n.jpg?_nc_cat=108&_nc_sid=09cbfe&_nc_ohc=8tazWz7VS5kAX_2zmY6&_nc_ht=scontent.fmia1-1.fna&oh=2878bcc67251e0a55ec869fd272e4abb&oe=5F8DDACA"
+          title="Chevy Hussain"
+        />
+      </div>
+    );
+}
+
+export default StoryReel
