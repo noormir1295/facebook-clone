4c0010d22d9b87f98e2b059f9d9c275e911923bb
Noor <noormir1295@gmail.com>
2020-09-24 19:50:52 -0400

    adding css to post section

--- fb-clone/src/Feed.js
+++ fb-clone/src/Feed.js
@@ -13,21 +13,21 @@ function Feed() {
 
         <Post
           profilePic="https://scontent.fmia1-1.fna.fbcdn.net/v/t1.0-9/42394604_2095447923798887_4400486546449367040_n.jpg?_nc_cat=101&_nc_sid=09cbfe&_nc_ohc=ijJMRrBehScAX8sZf_o&_nc_ht=scontent.fmia1-1.fna&oh=3d931dad43dbd71182d4785211c42499&oe=5F92815F"
-          message="This Works!!"
+          message="The new BMW M3"
           timestamp="this is a timestamp"
           username="noormir1295"
           image="https://cdn.bmwblog.com/wp-content/uploads/2020/09/2021-bmw-m3-competition-race-track-11.jpg"
         />
         <Post
           profilePic="https://scontent.fmia1-1.fna.fbcdn.net/v/t1.0-9/42394604_2095447923798887_4400486546449367040_n.jpg?_nc_cat=101&_nc_sid=09cbfe&_nc_ohc=ijJMRrBehScAX8sZf_o&_nc_ht=scontent.fmia1-1.fna&oh=3d931dad43dbd71182d4785211c42499&oe=5F92815F"
-          message="This Works!!"
+          message="Devonte Freeman signs with the New York Giants"
           timestamp="this is a timestamp"
           username="noormir1295"
           image="https://static.clubs.nfl.com/image/private/t_editorial_landscape_8_desktop_mobile/f_auto/giants/dmkcae520ksf1oba3p1o.jpg"
         />
         <Post
           profilePic="https://scontent.fmia1-1.fna.fbcdn.net/v/t1.0-9/42394604_2095447923798887_4400486546449367040_n.jpg?_nc_cat=101&_nc_sid=09cbfe&_nc_ohc=ijJMRrBehScAX8sZf_o&_nc_ht=scontent.fmia1-1.fna&oh=3d931dad43dbd71182d4785211c42499&oe=5F92815F"
-          message="This Works!!"
+          message="The all new Iphone 12 Pro"
           timestamp="this is a timestamp"
           username="noormir1295"
           image="https://cnet1.cbsistatic.com/img/chT5xA-aYgT0OyZojEImUmE41pY=/940x0/2019/11/07/09be7e4c-9801-4d03-b86d-248f501538ff/iphone-12-render-phone-arena-2.jpg"
