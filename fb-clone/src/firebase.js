54314f6c51c791b9ce30b79bdf6edbc4f5275eec
Noor <noormir1295@gmail.com>
2020-09-25 18:21:39 -0400

    connecting firbase db

--- fb-clone/src/firebase.js
+++ fb-clone/src/firebase.js
@@ -1,4 +1,5 @@
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+import firebase from "firebase";
 const firebaseConfig = {
   apiKey: "AIzaSyDcxWfHMxCoPbc2gHd_pRZIWTb8UkLgVG4",
   authDomain: "facebook-clone-4cf3e.firebaseapp.com",
@@ -9,3 +10,11 @@ const firebaseConfig = {
   appId: "1:626066219695:web:29b73ecee7d864642dea5a",
   measurementId: "G-NBFDTEBPYY",
 };
+
+const firebaseApp=firebase.initializeApp(firebaseConfig);
+const db=firebaseApp.firestore();
+const auth= firebase.auth();
+const provider=new firebase.auth.GoogleAuthProvider();
+
+export {auth, provider}
+export default db;
